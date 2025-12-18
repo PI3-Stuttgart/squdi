59ad9725dfb50e9d64d9f33dcb2ae90d1f5ebc7f
Sreehari Jayaram <Sriharijayaram5@gmail.com>
2025-12-18 14:02:31 +0100

    some updates

--- src/qudi/jupyternotebooks/channeling/zpl_distribution.py
+++ src/qudi/jupyternotebooks/channeling/zpl_distribution.py
@@ -5,21 +5,25 @@ import numpy as np
 import json
 import os
 from toptica.lasersdk.dlcpro.v2_0_3 import DLCpro,LaserHead,  NetworkConnection, DeviceNotFoundError
+scanning_probe_logic_ = galvo_scanning_probe_logic
+ple_gui_ = ple_gui_qinu
+scanner_gui_ = galvo_scanner_gui
+dl_pro_ = dl_pro_qinu
 def go_to_ple_target(target):
-    ple_gui._mw.ple_widget.target_point.setValue(target)
-    ple_gui._mw.ple_widget.target_point.sigPositionChangeFinished.emit(target)
+    ple_gui_._mw.ple_widget.target_point.setValue(target)
+    ple_gui_._mw.ple_widget.target_point.sigPositionChangeFinished.emit(target)
     #laser_scanner_logic.set_target_position({"a": target})
     time.sleep(0.5)
 
 def save_scan(name):
-    scanner_gui.save_path_widget.saveTagLineEdit.setText(name)
-    scanner_gui.scan_2d_dockwidgets[('x', 'y')].scan_widget.save_scan_button.clicked.emit() #saving
-    dir_ = scanning_data_logic.module_default_data_dir
+    scanner_gui_.save_path_widget.saveTagLineEdit.setText(name)
+    scanner_gui_.scan_2d_dockwidgets[('x', 'y')].scan_widget.save_scan_button.clicked.emit() #saving
+    dir_ = scanning_probe_logic_.module_default_data_dir
     return name, dir_
 
 #to be rapaired:
 def set_laser_offset(v):
-    with DLCpro(NetworkConnection(dl_pro.tcp_address)) as dlc:
+    with DLCpro(NetworkConnection(dl_pro_.tcp_address)) as dlc:
         dlc._laser1.dl.pc.voltage_set.set(v)
 
 ws_wavemeter.start_acquisition()
@@ -27,13 +31,13 @@ ws_wavemeter.start_acquisition()
 
 
 #%%
-dl_pro
+dl_pro_
 # %%
 # %%
 # Parameters
-offset_min_v = 45  # GHz to Hz
-offset_max_v = 95  # GHz to Hz
-steps =100
+offset_min_v = 10  # GHz to Hz
+offset_max_v = 80  # GHz to Hz
+steps = 20
 # num_steps = int((offset_max_v - offset_min_v) / step) + 1  # +1 for inclusive range
 
 w0 = 484.130 #THz
@@ -41,20 +45,20 @@ w0 = 484.130 #THz
 offsets = np.linspace(offset_min_v, offset_max_v, steps)
 #%%
 scan_names = {}
-experiment_name = "scan_10_10_158_170kev_Pplus"
+experiment_name = "scan_40_40_250_100kev-number5_deepest_trench_etched-total"
 # Loop through frequencies
 for i, v in enumerate(offsets):
     # Set laser frequency
     set_laser_offset(v)
-    time.sleep(1)
+    time.sleep(2)
 
     # Measure and record wavelength
     wavelength = (ws_wavemeter.get_current_wavelength() - w0) * 1e3   # to GHz
     print(wavelength)
-
+    time.sleep(1)
     # Start scan and save data
-    scanning_probe_logic.toggle_scan(True, ('x', 'y'))
-    while scanning_probe_logic.module_state()=='locked':
+    scanning_probe_logic_.toggle_scan(True, ('x', 'y'))
+    while scanning_probe_logic_.module_state()=='locked':
             time.sleep(1)
     
     name, dir_ = save_scan(f"{experiment_name}_{i}_{wavelength:.2f}GHz")
@@ -65,4 +69,16 @@ for i, v in enumerate(offsets):
 with open(f"{experiment_name}.txt", 'w') as file:
     json.dump(scan_names, file, indent=4)
 
+# %%
+for i, v in enumerate(offsets):
+    # Set laser frequency
+    set_laser_offset(v)
+    time.sleep(2)
+
+    # Measure and record wavelength
+    wavelength = (ws_wavemeter.get_current_wavelength() - w0) * 1e3   # to GHz
+    print(wavelength)
+
+
+    print(f"Scan {i+1}/{steps} completed for {wavelength:.2f} GHz")
 # %%
