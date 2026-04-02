6299284e04cea17d0fbc2d46e7ccc1551f3f69bc
vorobus <mrvorobus@gmail.com>
2026-04-02 11:44:45 +0200

    Update PLE_program.py

--- src/qudi/hardware/OPX/PLE_program.py
+++ src/qudi/hardware/OPX/PLE_program.py
@@ -86,7 +86,7 @@ def qm_scan_program(aoOPX):
                 play("pulse" * amp(volt_620 * 2), "Laser_620", duration=tt_trigger_len * u.ns)
                 with for_(i, 0, i < i_avg, i + 1):
                     play("pulse" * amp(volt_620 * 2), "Laser_620", duration=laser_pulse_len * u.ns)
-                    play("active", "Laser_620_pi", duration=laser_pulse_len * u.ns)
+                    # play("active", "Laser_620_pi", duration=laser_pulse_len * u.ns)
                     # play("trigit", "TT_attodry_trigger", duration=laser_pulse_len * u.ns)
                     # play("pulse" * amp(volt_520), "Laser_520", duration=laser_pulse_len)  # Time tagger stop trigger
                     # measure(
